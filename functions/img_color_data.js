c32fea679b75eb6e4db6993b8b7ee0351a120ffe
aisse-258 <y-kate@ukr.net>
2020-11-12 20:12:13 +0300

    Не учитываем (возможно цветные) края при вычислении среднего цвета изображения

--- functions/img_color_data.js
+++ functions/img_color_data.js
@@ -7,20 +7,24 @@ var img_color_data = function (image) {
 	for (let i = 0; i < image.bitmap.width; i++) {
 		bitDevs.push([]);
 	}
+	var widthDecile = Math.floor(image.bitmap.width*0.1);
+	var heightDecile = Math.floor(image.bitmap.height*0.1);
 	image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
 		let red = this.bitmap.data[idx + 0];
 		let green = this.bitmap.data[idx + 1];
 		let blue = this.bitmap.data[idx + 2];
-		redImg.push(red);
-		greenImg.push(green);
-		blueImg.push(blue);
+		if (x >= widthDecile && x < image.bitmap.width - widthDecile && y >= heightDecile && y < image.bitmap.height - heightDecile){
+			redImg.push(red);
+			greenImg.push(green);
+			blueImg.push(blue);
+		}
 		bitDevs[x][y] = {
 			'redGreenStd':Math.sqrt((red*red)/(red*red+green*green+blue*blue)),
 			'redBlueStd':Math.sqrt((green*green)/(red*red+green*green+blue*blue)),
 			'blueGreenStd':Math.sqrt((blue*blue)/(red*red+green*green+blue*blue))
 		};
 	});
-	let area = image.bitmap.width*image.bitmap.height;
+	let area = (image.bitmap.width - 2*widthDecile)*(image.bitmap.height - 2*heightDecile);
 	var imgDevs = {
 		'red': Math.floor(math.sum(redImg)/area),
 		'green': Math.floor(math.sum(greenImg)/area),
