650c326529a056a0d31eab36748288c442995b93
aisse-258 <y-kate@ukr.net>
2020-12-02 00:25:00 +0300

    Попытка веб-интерфейса

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
 const http = require('http');
 
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+var Jimp = require('jimp');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+//const { JSDOM } = require( "jsdom" );
+//const { window } = new JSDOM( "" );
+//const $ = require( "jquery" )( window );
+//var $ = require('jquery-with-bootstrap-for-browserify');
+var childProcess = require('child_process');
+//var main = fs.readFileSync('./webui/index.html','utf-8');
+const server = express();
+var onloadOrig = fs.readFileSync('./webui/onload.html','utf-8');
+
+server.use(express.static(__dirname));
+server.use(multer({dest:"uploads"}).single("filedata"));
+server.post("/upload",function(req,res,next){
+	let filedata = req.file;
+	console.log(filedata);
+	if(!filedata){
+		res.send("error on load");
+	}
+	else {
+		res.setHeader('Content-Type', 'text/html');
+		var treshold = 2;//1 * $('#treshold').val();
+		var divH = 1,//1 * $('#divide-height').val(),
+		divW = 1;//1 * $('#divide-width').val();
+		var grid1 = 16,//1 * $('#grid1').val(),
+		grid2 = 32;//1 * $('#grid2').val();
+		childProcess.exec('node retry.js \'' + JSON.stringify(filedata) + '\' ' +
+		treshold + ' ' + divH + 'x' + divW + ' ' + grid1 + ' ' + grid2,
+		function(err, stdout, stderr){
+			console.log(err,stdout,stderr);
+			Jimp.read(filedata.path,(err, image) => {
+				var onload = onloadOrig.replace(/image-name/,filedata.filename);
+				onload = onload.replace(/image-mod-name/,filedata.originalname.slice(0,-4)+'_mod.jpg');
+				onload = onload.replace(/image-height/g,String(image.bitmap.height*0.3));
+				onload = onload.replace(/image-width/g,String(image.bitmap.width*0.3));
+				res.send(onload);
+			});
+		});
+	}
 });
+/*server.get('/download',function(req, res){
+	const file = `${__dirname}/upload/img.jpg`;
+	res.download(file);
+});
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(main);
+});*/
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
--- retry.js
+++ retry.js
@@ -1,8 +1,10 @@
 var Jimp = require('jimp');
 var math = require('mathjs');
 var childProcess = require('child_process');
-var img = process.argv[2];
+var img = JSON.parse(process.argv[2]);
 var divide = process.argv[4] || '1x1';
+var grid1 = Number(process.argv[5]), grid2 =  Number(process.argv[6]);
+var fs = require('fs');
 
 var find_plain = require('./functions/find_plain.js');
 var img_color_data = require('./functions/img_color_data.js');
@@ -11,26 +13,25 @@ var colors_to_paper = require('./functions/colors_to_paper.js');
 var simple_colors = require('./functions/simple_colors.js');
 
 //var t = Date.now();
-
-Jimp.read(img, (err, image) => {
+Jimp.read(img.path, (err, image) => {
 if (err) throw err;
 	var imageColorData = img_color_data(image, Number(divide.slice(0,1)), divide.slice(-1));
-	var grid1 = 32, grid2 = 16;
 	var threshold = Number(process.argv[3]);
 	simple_colors(image, grid1, 8);
 	var simpleMap = simple_colors(image, grid2, 10);
 	colors_to_paper(image, imageColorData, simpleMap, grid2, threshold);
 	//find_plain(image, 8, 20);
-	image.write(img.slice(0,-4) + '_mod.bmp', function() {
-		if (img.slice(-4) == '.jpg') {
-			childProcess.execSync('convert ' + img.slice(0,-4) + '_mod.bmp -quality ' +
-			childProcess.execSync('identify -format \'%Q\' ' + img).toString() + ' ' + img.slice(0,-4) + '_mod.jpg');
+	fs.writeFileSync('test.txt','test');
+	image.write('./uploads/'+img.originalname.slice(0,-4) + '_mod.bmp', function() {
+		if (img.originalname.slice(-4) == '.jpg') {
+			childProcess.execSync('convert ' +'./uploads/'+ img.originalname.slice(0,-4) + '_mod.bmp -quality ' +
+			childProcess.execSync('identify -format \'%Q\' ' + img.path).toString() + ' ' +'./uploads/'+ img.originalname.slice(0,-4) + '_mod.jpg');
 			//console.log(Date.now()-t);
 		}
 		else {
-			childProcess.execSync('convert ' + img.slice(0,-4) + '_mod.bmp ' + img.slice(0,-4) + '_mod.jpg');
+			childProcess.execSync('convert ' +'./uploads/'+ img.originalname.slice(0,-4) + '_mod.bmp ' +'./uploads/'+ img.originalname.slice(0,-4) + '_mod.jpg');
 			//console.log(Date.now()-t);
 		}
-		childProcess.execSync('rm ' + img.slice(0,-4) + '_mod.bmp');
+		childProcess.execSync('rm ' +'./uploads/'+ img.originalname.slice(0,-4) + '_mod.bmp');
 	});
 });
