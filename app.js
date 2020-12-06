bb8d14d71d36a3939a7629fffbd76269674c71d7
aisse-258 <y-kate@ukr.net>
2020-12-06 21:55:22 +0300

    Обработка нескольких изображений

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ const server = express();
 var onloadOrig = fs.readFileSync('./webui/onload_template.html','utf-8');
 
 server.use(express.static(__dirname));
-server.use(multer({dest:"uploads"}).single("filedata"));
+server.use(multer({dest:"uploads"}).array("filedata"));
 server.post("/upload",function(req,res,next){
-	let filedata = req.file;
-	//console.log(req.body);
+	let filedata = req.files;
+	console.log(req);
 	if(!filedata){
 		res.send("error on load");
 	}
@@ -32,19 +32,29 @@ server.post("/upload",function(req,res,next){
 		var simplifyAreas = req.body['simplify-areas'] == 'on' ? '' : ' --no-simplify ';
 		var grid1 = req.body['simplify-areas'] == 'on' && req.body['simplify-big-areas'] == 'on' ? req.body.grid1 : 0,
 		grid2 = req.body['simplify-areas'] == 'on' ? req.body.grid2 : req.body.grid;
-		childProcess.exec('node retry.js -t ' + treshold + ' -h ' + divH + ' -w ' + divW +
-		' -G ' + grid1 + ' -g ' + grid2 + simplifyAreas +
-		' -- \'' + JSON.stringify(filedata) + '\'',
-		function(err, stdout, stderr){
+		var command = '';
+		var imgsShow = '';
+		for(let i = 0; i < filedata.length; i++){
+			command += 'node retry.js -t ' + treshold + ' -h ' + divH + ' -w ' + divW +
+			' -G ' + grid1 + ' -g ' + grid2 + simplifyAreas +
+			' -- \'' + JSON.stringify(filedata[i]) + '\' & ';
+
+			imgsShow += '<div id="img-res'+i+'">\n'+
+			'<img src="../uploads/'+filedata[i].filename+'" style="max-width: 49%;">\n'+
+			'<a href="'+filedata[i].originalname.slice(0,-4)+'_mod.jpg" download="'+
+			filedata[i].originalname.slice(0,-4)+'_mod.jpg">\n'+
+			'<img src="../uploads/'+filedata[i].originalname.slice(0,-4)+'_mod.jpg'+'" style="max-width: 49%;"></a><br>\n'+
+			'<a href="'+filedata[i].originalname.slice(0,-4)+'_mod.jpg" download="'+
+			filedata[i].originalname.slice(0,-4)+
+			'_mod.jpg">Скачать изображение</a>'+
+			'\n</div>\n';
+		}
+		command += 'wait';
+		childProcess.exec(command, function(err, stdout, stderr){
 			console.log(err,stdout,stderr);
-			Jimp.read(filedata.path,(err, image) => {
-				var onload = onloadOrig.replace(/image-name/,filedata.filename);
-				onload = onload.replace(/image-mod-name/,filedata.originalname.slice(0,-4)+'_mod.jpg');
-				onload = onload.replace(/image-height/g,String(image.bitmap.height*0.3));
-				onload = onload.replace(/image-width/g,String(image.bitmap.width*0.3));
-				fs.writeFileSync('./webui/onload.html',onload);
-				res.send(onload);
-			});
+			var onload = onloadOrig.replace(/insert-imgs-here/,imgsShow);
+			//fs.writeFileSync('./webui/onload.html',onload);
+			res.send(onload);
 		});
 	}
 });
