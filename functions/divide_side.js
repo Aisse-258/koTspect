c499da9c0feeaa5cca4a3dd7fce4a456f9c388ea
aisse-258 <y-kate@ukr.net>
2020-12-24 18:49:16 +0300

    Действие функции в нестандартных случаях; пробелы в табы

--- functions/divide_side.js
+++ functions/divide_side.js
@@ -1,16 +1,22 @@
 var divide_side = function(sideLength, partCount) {
-    if(partCount > sideLength) {
-        return [0,sideLength];
-    }
-    var points = [0];
-    var s=0;
-    for(let i=0;i<partCount;i++){
-        s+=Math.floor(sideLength/partCount);
-        //console.log(s);
-        points.push(s);
-    }
-    points[points.length-1]+=sideLength%partCount;
-    return points;
+	if (Math.round(partCount) != partCount) {
+		partCount = Math.round(partCount);
+	}
+	if (partCount < 0) {
+		partCount = Math.abs(partCount);
+	}
+	if(partCount > sideLength || partCount <= 1) {
+		return [0,sideLength];
+	}
+	var points = [0];
+	var s=0;
+	for(let i=0;i<partCount;i++){
+		s+=Math.floor(sideLength/partCount);
+		//console.log(s);
+		points.push(s);
+	}
+	points[points.length-1]+=sideLength%partCount;
+	return points;
 }
 
 module.exports = divide_side;
